a1a5f8851d8b6d4d7b63d6a2402fd5e8311f610f
Paola Rosano <paodiazglez.pdgr@gmail.com>
2022-09-26 10:51:44 -0500

    join the welcome, register and login layout

--- src/components/wall.js
+++ src/components/wall.js
@@ -1,3 +1,4 @@
+import { onNavigate } from '../main.js';
 import { endSesion } from '../lib/auth.js';
 
 export const wall = () => {
@@ -34,7 +35,10 @@ export const wall = () => {
   logOut.classList.add('logOut');
 
   logOut.addEventListener('click', () => {
-    endSesion();
+    endSesion()
+      .then(() => {
+        onNavigate('/');
+      });
   });
   upperBanner.append(growLetters, textUserName, userIcon);
   postsSection.append(publishedPost);
