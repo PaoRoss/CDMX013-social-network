4f08bab67af647d4a3a1a021240fff2a27ef2918
Marian Villavicencio <marian.mlva.16@gmail.com>
2022-09-27 12:40:00 -0500

    feat: Show email in wall layout

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { welcome } from './components/welcome.js';
 import { register } from './components/register.js';
 import { LogOn } from './components/signin.js';
 import { wall } from './components/wall.js';
+import { onAuthStateChanged, getAuth } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-auth.js';
 
 const root = document.getElementById('root');
 const routes = {
@@ -29,3 +30,11 @@ window.onpopstate = () => {
 };
 
 root.appendChild(component());
+
+onAuthStateChanged(getAuth(), (user) =>{
+  if(user){
+    onNavigate('/wall');
+  } else{
+    onNavigate('/');
+  }
+});
